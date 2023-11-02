4771aa974588146b84c0e9cd034b58f74b8e59ca
Kheiria-Hamayel <khiriyya123.amjad@gmail.com>
2023-11-02 22:39:46 +0200

    L25-L27

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
@@ -25,6 +26,10 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              {/*id of blog */}
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
